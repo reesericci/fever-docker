e45441f964cd4d121ecfc35882ad9e76fae2711d
Reese Armstrong <me@reeseric.ci>
2024-03-04 22:10:19 -0600

    Create db.php

---
+++ config/db.php
@@ -0,0 +1,7 @@
+<?php
+define("FEVER_DB_SERVER", "127.0.0.1");
+define("FEVER_DB_DATABASE", "fever");
+define("FEVER_DB_USERNAME", "fever");
+define("FEVER_DB_PASSWORD", "feedafever");
+define("FEVER_DB_PREFIX","");
+?>
